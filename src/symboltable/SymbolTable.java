1021f29213f5d375c049924de1e111cf46e82521
SapphireDragoness <linda.monfermoso@gmail.com>
2024-01-14 18:55:10 +0100

    implementazione symbol table

--- src/symboltable/SymbolTable.java
+++ src/symboltable/SymbolTable.java
@@ -12,7 +12,17 @@ public class SymbolTable {
 		
 		private LangType tipo;
 		
-		
+		public Attributes(LangType tipo) {
+			this.setTipo(tipo);
+		}
+
+		public LangType getTipo() {
+			return tipo;
+		}
+
+		public void setTipo(LangType tipo) {
+			this.tipo = tipo;
+		}
 		
 	}
 
@@ -21,19 +31,28 @@ public class SymbolTable {
 	}
 
 	public static boolean enter(String id, Attributes entry) {
-// ......
+		if(symbolTable.get(id) != null) {
+			return false;
+		}
+		symbolTable.put(id, entry);
+		return true;
 	}
 
 	public static Attributes lookup(String id) {
-//......
+		return symbolTable.get(id);
 	}
 
 	public static String toStr() {
-// ......
+		StringBuilder str = new StringBuilder();
+		
+		for(var e : symbolTable.entrySet()) {
+			str.append("Chiave").append(e.getKey()).append("\t").append("Valore").append(e.getValue()).append("\n");
+		}
+		return str.toString();
 	}
 
 	public static int size() {
-// ......
+		return symbolTable.size();
 	}
 	
 }
