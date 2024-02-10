27e455e42119a6f4aaf4555fd17f85b819fea09e
SapphireDragoness <linda.monfermoso@gmail.com>
2024-02-10 19:23:43 +0100

    modifica eccezioni per renderle più espressive

--- src/eccezioni/LexicalException.java
+++ src/eccezioni/LexicalException.java
@@ -2,12 +2,16 @@ package eccezioni;
 
 public class LexicalException extends Exception {
 	
-	public LexicalException() {
-		super();
+	public LexicalException(int riga, char c) {
+		super("Errore lessicale a riga " + riga + ": carattere illegale " + c);
 	}
 	
-	public LexicalException(String msg) {
-		super(msg);
+	public LexicalException(int riga, String id) {
+		super("Errore lessicale a riga " + riga + ": sequenza di caratteri '" + id + "' non riconosciuta");
+	}
+
+	public LexicalException(int riga) {
+		super("Errore lessicale a riga " + riga + ": impossibile leggere carattere");
 	}
 	
 }
--- src/eccezioni/SyntacticException.java
+++ src/eccezioni/SyntacticException.java
@@ -1,9 +1,11 @@
 package eccezioni;
 
+import token.TokenType;
+
 public class SyntacticException extends Exception {
 	
-	public SyntacticException() {
-		super();
+	public SyntacticException(int riga, String atteso, TokenType ottenuto) {
+		super("Errore sintattico a riga " + riga + ": atteso " + atteso + ", ma è " + ottenuto);
 	}
 	
 	public SyntacticException(String msg) {
