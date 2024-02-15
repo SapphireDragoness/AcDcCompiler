3376c3bfe456529368fb075ac81b538523276dce
SapphireDragoness <linda.monfermoso@gmail.com>
2024-02-15 12:36:13 +0100

    aggiunta JavaDoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"symboltable","l":"SymbolTable.Attributes"},{"p":"symboltable","l":"SymbolTable"}];updateSearchResults();
